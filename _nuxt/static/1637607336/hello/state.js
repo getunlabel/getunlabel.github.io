03d7b63d6995c41e06c66d909ce55bcba86a9248
thombruce <thombruce@users.noreply.github.com>
2021-11-22 18:55:40 +0000

    deploy: b00954504d2e96bfbbba2ee73365fd62a3959e87

---
+++ _nuxt/static/1637607336/hello/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637607336",layout:"docs",error:a,serverRendered:true,routePath:"\u002Fhello",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ba1d484f"}}}}(null));
